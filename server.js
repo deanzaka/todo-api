b76a327e3c4741dacdcda6d3cb2492b181da1a98
Dean Zaka Hidayat <deanzaka.h@gmail.com>
2017-07-22 22:51:33 +0700

    UpdatePUT /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/todos', function (req, res) {
 app.get('/todos/:id', function (req, res) {
   var todoId = parseInt(req.params.id, 10);
   db.todo.findById(todoId).then(function(todo) {
-    if(!!todo) {
+    if(todo) {
       res.json(todo.toJSON());
     } else {
       res.status(404).send("No to do found for supplied Id");
@@ -86,28 +86,30 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {id: todoId});
   var body = _.pick(req.body, 'description', 'completed');
-  var validAttributes = {};
+  var attributes = {};
 
-  if(!matchedTodo) {
-    return res.status(404).send();
+  if(body.hasOwnProperty('completed')) {
+    attributes.completed = body.completed;
   }
 
-  if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-    validAttributes.completed = body.completed;
-  } else if (body.hasOwnProperty('completed')) {
-    return res.status(400).send();
+  if(body.hasOwnProperty('description')) {
+    attributes.description = body.description.trim();
   }
 
-  if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-    validAttributes.description = body.description.trim();
-  } else if (body.hasOwnProperty('description')) {
-    return res.status(400).send();
-  }
-
-  _.extend(matchedTodo, validAttributes);
-  res.json(matchedTodo);
+  db.todo.findById(todoId).then(function(todo){
+    if(todo) {
+      todo.update(attributes).then(function(todo) {
+        res.json(todo.toJSON());
+      }, function(e) {
+        res.status(400).json(e);
+      });
+    } else {
+      res.status(404).send();
+    }
+  }, function(e) {
+    res.status(500).json(e);
+  });
 });
 
 db.sequelize.sync().then(function() {
