2389e9a2a991f2243e76eb8b7dec57e679d3f17e
Dean Zaka Hidayat <deanzaka.h@gmail.com>
2017-07-23 10:24:18 +0700

    Adding todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,46 +19,39 @@ var Todo = sequelize.define('todo', {
   }
 });
 
+var User = sequelize.define('user', {
+  email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
   // force: true
 }).then(function() {
   console.log('Everything is synced');
 
-  Todo.findById(2).then(function(todo) {
-    if (todo) {
-      console.log('Here we go');
-      console.log(todo.toJSON());
-    } else {
-      console.log('No todo found by that Id');
-    }
+  User.findById(1).then(function(user) {
+    user.getTodos({
+      where: {
+        completed: true
+      }
+    }).then(function(todos) {
+      todos.forEach(function(todo) {
+        console.log(todo.toJSON());
+      });
+    });
   });
 
-
-  // Todo.create({
-  //   description: 'Do something here',
-  //   completed: false
+  // User.create({
+  //   email: 'deanzaka.h@gmail.com'
   // }).then(function() {
   //   return Todo.create({
-  //     description: 'Learn to code better'
+  //     description: 'check out asociation'
   //   });
-  // }).then(function() {
-  //   // return Todo.findById(1);
-  //   return Todo.findAll({
-  //     where: {
-  //       description: {
-  //         $like: '%code%'
-  //       }
-  //     }
+  // }).then(function(todo) {
+  //   User.findById(1).then(function(user) {
+  //     user.addTodo(todo);
   //   });
-  // }).then(function(todos) {
-  //   if(todos) {
-  //     todos.forEach(function(todo) {
-  //       console.log(todo.toJSON());
-  //     });
-  //   } else {
-  //     console.log('no todo found');
-  //   }
-  // }).catch(function (e) {
-  //   console.log(e);
   // });
 });
