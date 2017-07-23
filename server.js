d80caa811fbbdda1ac4cb7f91197f631a70917c6
Dean Zaka Hidayat <deanzaka.h@gmail.com>
2017-07-23 10:44:49 +0700

    Convert CRUD request to use current user only

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get('/', function (req, res) {
 // GET /todos?completed=true&q=keyword
 app.get('/todos', middleware.requireAuthentication, function (req, res) {
   var query = req.query;
-  var where = {};
+  var where = {
+    userId: req.user.get('id')
+  };
 
   if(query.hasOwnProperty('completed')) {
       where.completed = (query.completed.toLowerCase() === "true");
@@ -40,7 +42,12 @@ app.get('/todos', middleware.requireAuthentication, function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', middleware.requireAuthentication, function (req, res) {
   var todoId = parseInt(req.params.id, 10);
-  db.todo.findById(todoId).then(function(todo) {
+  db.todo.findOne({
+    where: {
+      id: todoId,
+      userId: req.user.get('id')
+    }
+  }).then(function(todo) {
     if(todo) {
       res.json(todo.toJSON());
     } else {
@@ -73,7 +80,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
   var todoId = parseInt(req.params.id, 10);
   db.todo.destroy({
     where: {
-      id: todoId
+      id: todoId,
+      userId: req.user.get('id')
     }
   }).then(function(todo) {
     if(todo > 0) {
@@ -102,7 +110,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
     attributes.description = body.description.trim();
   }
 
-  db.todo.findById(todoId).then(function(todo){
+  db.todo.findOne({
+    where: {
+      id: todoId,
+      userId: req.user.get('id')
+    }
+  }).then(function(todo){
     if(todo) {
       todo.update(attributes).then(function(todo) {
         res.json(todo.toJSON());
